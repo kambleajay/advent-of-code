1ccea28f2ce8e53fa9a5abf6e0f2097c5613068c
Ajay Kamble <ajay.kamble@selfscore.com>
2021-01-25 16:02:22 +0530

    solution for day 2

---
+++ src/aoc/day2.clj
@@ -0,0 +1,37 @@
+(ns aoc.day2
+  (:require [clojure.java.io :as io]))
+
+(defn valid-password? [{:keys [min-occurrence max-occurrence ch password]}]
+  (let [count-freq (fn [acc next-ch] (if (= (str next-ch) ch) (inc acc) acc))
+        ch-freq (reduce count-freq 0 password)]
+    (and (>= ch-freq min-occurrence) (<= ch-freq max-occurrence))))
+
+(defn parse-policy-and-password [s]
+  (let [pattern #"(\d+)-(\d+)\s(.):\s(.+)"
+        [_ min-occ max-occ ch password] (re-matches pattern s)]
+    {:min-occurrence (Integer/parseInt min-occ)
+     :max-occurrence (Integer/parseInt max-occ)
+     :ch             ch
+     :password       password}))
+
+(defn valid-password-alt? [{:keys [index-1 index-2 ch password]}]
+  (let [index-1-ch (str (nth password (dec index-1)))
+        index-2-ch (str (nth password (dec index-2)))]
+    (or (and (= index-1-ch ch) (not= index-2-ch ch))
+        (and (not= index-1-ch ch) (= index-2-ch ch)))))
+
+(defn parse-policy-and-password-alt [s]
+  (let [pattern #"(\d+)-(\d+)\s(.):\s(.+)"
+        [_ index-1 index-2 ch password] (re-matches pattern s)]
+    {:index-1 (Integer/parseInt index-1)
+     :index-2 (Integer/parseInt index-2)
+     :ch             ch
+     :password       password}))
+
+(defn answer [parse-fn valid-fn]
+  (with-open [rdr (io/reader (io/resource "day2.txt"))]
+    (let [lines (line-seq rdr)]
+      (->> lines
+           (map parse-fn)
+           (filter valid-fn)
+           count))))
